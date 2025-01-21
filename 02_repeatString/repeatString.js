2333c693ad2a2325e1d5e953e22202c242315103
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-21 15:54:30 +0300

    Add loop to repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num){
+    if (num < 0){
+        return 'ERROR';
+    }
+    string = '';
+    for (let i = 0; i < num; i++){
+        string.repeat(num)
+    }
+    return string;
 };
 
 // Do not edit below this line
