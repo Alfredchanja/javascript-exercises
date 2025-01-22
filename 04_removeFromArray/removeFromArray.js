864c82598bc0b254470a9cec99cc80c89caf0b54
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-22 19:56:40 +0300

    Add code that removes items from multiple optional arguments.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value, ...arg){
+    // Combines values and multiple optional arguments into one list.
+    const valuesToRemove = [value, ...arg];
+
+    // Loops through the list of the values to be removed.
+    for (let i = 0; i < valuesToRemove.length; i++){
+        let val = valuesToRemove[i];
+        let index = arr.indexOf(val);
+
+        // Removes all occurrences of the value in 'arr.' 
+        while (index > -1){
+            arr.splice(index, 1);
+            index = arr.indexOf(val);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
