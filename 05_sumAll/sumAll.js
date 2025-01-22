522acb72f41f7d56191be43c6568819721b38ac9
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-22 21:18:59 +0300

    Returns the sum of a range of numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumOfAll = 0;
 
+    if (num1 < 1 || num2 < 1 || num1 !== Math.floor(num1) || num2 !== Math.floor(num2)){
+        return 'ERROR';
+    }else if(typeof num1 !== "number" || typeof num2 !== "number" ||
+        !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    } else if (num1 > num2) {
+        for(let i = num1; i >= num2 ; i--){
+            sumOfAll += i
+        }
+    }else {
+        for( let i = num1; i < num2 + 1; i++){
+            sumOfAll += i;
+        }
+    }
+
+    return sumOfAll;
 };
 
 // Do not edit below this line
