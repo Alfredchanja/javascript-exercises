e7745c0cf1e9d8b41392dcc84e4eb20c0abb0b85
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-23 18:03:06 +0300

    Check for palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (char) {
+    char = char.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    for (let i = 0; i < char.length - 1; i++) {
+        if (char[i] !== char[char.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
