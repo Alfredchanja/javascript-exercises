0225c89b690dbad15490d5e68616c75c7d0983f9
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-23 22:37:40 +0300

    Create a fibonacci calculator.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = 0;
 
+    if (num < 0) {
+        return "OOPS";
+    } else if (typeof num === "string"){
+        num = Number(num);
+    }
+
+    let a = 1, b = 1;
+    for (let i = 0; i <= num; i++) {
+        if (i === 0) {
+            fib = 0;
+        } else if (i === 1) {
+            fib = 1;
+        } else {
+            fib = b;
+            b = a + b;
+            a = fib;
+        }
+    }
+    return fib;
 };
 
 // Do not edit below this line
