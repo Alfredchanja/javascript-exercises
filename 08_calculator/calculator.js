da91e2c2dd06099b51187c1ea5fb8e467b656bd6
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-23 17:01:59 +0300

    Create a simple javascript calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	const sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	const difference = num1 - num2;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	let total = 0;
+  for (let i = 0; i < num.length; i++) {
+    total += num[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let product = 1;
+  for (let i = 0; i < num.length; i++) {
+    product *= num[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let power = Math.pow(num1, num2);
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
