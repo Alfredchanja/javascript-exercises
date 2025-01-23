cc6d8cec444e8d6985c8114d4e608c38526dd057
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-23 13:27:27 +0300

    Check if year is leap or not.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Not all years that are divisible by 4 are divisible by 400.
+    // If the year is either divisible by 4 or 400 it is a leap year.
+    // Not all years that are divisible by 100 are leap years.
+    if (year % 4 !== 0 || year % 400 !== 0 && year % 100 === 0) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 // Do not edit below this line
