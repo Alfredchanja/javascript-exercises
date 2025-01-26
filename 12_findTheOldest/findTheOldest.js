75a907f348c6a328c6fc005b7104d5228cd30987
Alfredchanja <alfredgachanja8@gmail.com>
2025-01-26 13:57:12 +0300

    Create a function that finds the oldest person with the year of birth and death or current year

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArr) {
+    let oldest = objArr[0];
+    let age = 0;
+    for (let i = 0; i < objArr.length; i++) {
+        let birthYear = objArr[i].yearOfBirth;
+        let deathYear = objArr[i].yearOfDeath || new Date().getFullYear();
 
+        age = deathYear - birthYear;
+        objArr[i].age = age;
+
+        if (objArr[i].age > oldest.age) {
+            oldest = objArr[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
